e8365c7be676efe25cb4b7ca15bb27ce2a60b14b
iiTzBenni <161487331+iiTzBenni@users.noreply.github.com>
2024-04-28 15:44:34 +0700

    Update trolling.lua

--- trolling.lua
+++ trolling.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "V1rtual Hub - FR ScriptHub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", 
+local Window = OrionLib:MakeWindow({Name = "V1rtual Hub - FE ScriptHub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", 
 IntroText = "Made by Benny"})
 
 OrionLib:MakeNotification({
@@ -22,7 +22,7 @@ local Section = Local:AddSection({
 
 Local:AddSlider({
 	Name = "WalkSpeed",
-	Min = 16,
+	Min = 0,
 	Max = 500,
 	Default = 16,
 	Color = Color3.fromRGB(131,105,83),
@@ -35,7 +35,7 @@ Local:AddSlider({
 
 Local:AddSlider({
 	Name = "Jump Power",
-	Min = 50,
+	Min = 0,
 	Max = 500,
 	Default = 50,
 	Color = Color3.fromRGB(119, 221, 119),
@@ -48,14 +48,90 @@ Local:AddSlider({
 
 Local:AddSlider({
 	Name = "Time",
-	Min = 14,
+	Min = 0,
 	Max = 24,
 	Default = 14,
-	Color = Color3.fromRGB(119, 221, 119),
+	Color = Color3.fromRGB(255, 179, 71),
 	Increment = 1,
 	ValueName = "Time",
 	Callback = function(Value)
-        game.GetService.Lightning.TimeofDay = (Value)
+        game.Lighting.ClockTime = UDim.new(Value / 24, 0)
+	end    
+})
+
+Local:AddSlider({
+	Name = "Gravity",
+	Min = 0,
+	Max = 500,
+	Default = 196,
+	Color = Color3.fromRGB(254, 201, 143),
+	Increment = 1,
+	ValueName = "Gravity",
+	Callback = function(Value)
+        workspace.Gravity = (Value)
+	end    
+})
+
+Local:AddParagraph("Note:","Sliders may not work for mobile, scroll all the way down if you're on mobile")
+
+Local:AddButton({
+	Name = "Respawn",
+	Callback = function()
+        game.Players.LocalPlayer.Character:Destroy()
+		game.Players.LocalPlayer:LoadCharacter()
+  	end    
+})
+
+Local:AddButton({
+	Name = "Rejoin",
+	Callback = function()
+		game.Players.LocalPlayer:Kick("Rejoining...")
+		wait(1)
+		game.Players.LocalPlayer:Rejoin()
+	end    
+})
+
+Local:AddButton({
+	Name = "Auto Rejoin",
+	Callback = function()
+		game.Players.LocalPlayer.Kick:Connect(function()
+            wait(1)
+            game.Players.LocalPlayer:Rejoin()
+        end)
+	end    
+})
+
+Local:AddButton({
+	Name = "Join Smallest Server",
+	Callback = function()
+		local smallestServer = nil
+		local smallestPlayerCount = math.huge
+		for _, server in ipairs(game.Servers:GetChildren()) do
+			if server:FindFirstChild("Players") then
+				local playerCount = server.Players.PlayerCount.Value
+				if playerCount < smallestPlayerCount then
+					smallestServer = server
+					smallestPlayerCount = playerCount
+				end
+			end
+		end
+		if smallestServer then
+			game.Players.LocalPlayer:MoveTo(smallestServer)
+		end
+	end    
+})
+
+Local:AddButton({
+	Name = "Server Finder",
+	Callback = function()
+		loadstring(game:HttpGet("https://www.scriptblox.com/raw/Server-Browser_80", true))();
+	end    
+})
+
+Local:AddButton({
+	Name = "Noclip",
+	Callback = function()
+		game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
 	end    
 })
 
