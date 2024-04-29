5b7be5a35ffff1905714adee5f8be1d42d61c3c8
iiTzBenni <161487331+iiTzBenni@users.noreply.github.com>
2024-04-29 21:22:51 +0700

    Update virtual-trolling.lua

--- virtual-trolling.lua
+++ virtual-trolling.lua
@@ -1,64 +1,65 @@
+--Library
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-
+-- Window
 local Window = OrionLib:MakeWindow({Name = "V1rtual Hub - FE ScriptHub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", 
 IntroText = "V1rtual Hub"})
-
+--notification
 OrionLib:MakeNotification({
-	Name = "Thanks for using!",
-	Content = "Made by Benny",
-	Image = "rbxassetid://4483345998",
-	Time = 5
+  Name = "Thanks for using!",
+  Content = "Made by Benny",
+  Image = "rbxassetid://4483345998",
+  Time = 5
 })
-
+--Local tab Section
 local Local = Window:MakeTab({
-	Name = "Local",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+  Name = "Local",
+  Icon = "rbxassetid://4483345998",
+  PremiumOnly = false
 })
 
 local Section = Local:AddSection({
-	Name = "Local"
+  Name = "Local"
 })
 
 Local:AddParagraph("Note:","Sliders may not work for mobile, scroll down a bit if you're on mobile")
 
 Local:AddSlider({
-	Name = "WalkSpeed",
-	Min = 0,
-	Max = 500,
-	Default = 16,
-	Color = Color3.fromRGB(131,105,83),
-	Increment = 1,
-	ValueName = "Speed",
-	Callback = function(Value)
+  Name = "WalkSpeed",
+  Min = 0,
+  Max = 500,
+  Default = 16,
+  Color = Color3.fromRGB(131,105,83),
+  Increment = 1,
+  ValueName = "Speed",
+  Callback = function(Value)
         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
-	end    
+  end    
 })
 
 Local:AddSlider({
-	Name = "Jump Power",
-	Min = 0,
-	Max = 500,
-	Default = 50,
-	Color = Color3.fromRGB(119, 221, 119),
-	Increment = 1,
-	ValueName = "Power",
-	Callback = function(Value)
+  Name = "Jump Power",
+  Min = 0,
+  Max = 500,
+  Default = 50,
+  Color = Color3.fromRGB(119, 221, 119),
+  Increment = 1,
+  ValueName = "Power",
+  Callback = function(Value)
         game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
-	end    
+  end    
 })
 
 Local:AddSlider({
-	Name = "Gravity",
-	Min = 0,
-	Max = 500,
-	Default = 196,
-	Color = Color3.fromRGB(254, 201, 143),
-	Increment = 1,
-	ValueName = "Gravity",
-	Callback = function(Value)
+  Name = "Gravity",
+  Min = 0,
+  Max = 500,
+  Default = 196,
+  Color = Color3.fromRGB(254, 201, 143),
+  Increment = 1,
+  ValueName = "Gravity",
+  Callback = function(Value)
         workspace.Gravity = (Value)
-	end    
+  end    
 })
 
 
@@ -73,140 +74,140 @@ Local:AddButton({
 })
 
 Local:AddButton({
-	Name = "Rejoin",
-	Callback = function()
+  Name = "Rejoin",
+  Callback = function()
         loadstring(game:HttpGet("https://pastefy.app/R5X0t5b3/raw"))()
-	end    
+  end    
 })
 
 Local:AddButton({
-	Name = "Auto Rejoin",
-	Callback = function()
+  Name = "Auto Rejoin",
+  Callback = function()
         loadstring(game:HttpGet("https://pastefy.app/iWcPBbBD/raw"))()
-	end    
+  end    
 })
 
 Local:AddButton({
-	Name = "Join Smallest Server",
-	Callback = function()
+  Name = "Join Smallest Server",
+  Callback = function()
         loadstring(game:HttpGet("https://pastefy.app/B7QW3AE7/raw"))()
-	end    
+  end    
 })
 
 Local:AddButton({
-	Name = "Server Finder",
-	Callback = function()
-		loadstring(game:HttpGet("https://www.scriptblox.com/raw/Server-Browser_80", true))();
-	end    
+  Name = "Server Finder",
+  Callback = function()
+    loadstring(game:HttpGet("https://www.scriptblox.com/raw/Server-Browser_80", true))();
+  end    
 })
 
 Local:AddButton({
-	Name = "Noclip",
-	Callback = function()
-		loadstring(game:HttpGet("https://pastefy.app/miBjKMup/raw"))()
-	end    
+  Name = "Noclip",
+  Callback = function()
+    loadstring(game:HttpGet("https://pastefy.app/miBjKMup/raw"))()
+  end    
 })
 
 Local:AddButton({
-	Name = "Anti-AFK",
-	Callback = function()
-		loadstring(game:HttpGet("https://pastefy.app/4DUFd5xu/raw"))()
-	end    
+  Name = "Anti-AFK",
+  Callback = function()
+    loadstring(game:HttpGet("https://pastefy.app/4DUFd5xu/raw"))()
+  end    
 })
 
 
 
 local Section = Local:AddSection({
-	Name = "Utility"
+  Name = "Utility"
 })
 
 Local:AddButton({
-	Name = "BTools (SS)",
-	Callback = function()
-		loadstring(game:GetObjects("rbxassetid://6695644299")[1].Source)()
-	end    
-})
-  
-Local:AddButton({
-	Name = "XRay",
-	Callback = function()
-		loadstring(game:HttpGet("https://pastefy.app/wmcnqSrX/raw"))()
-	end    
+  Name = "BTools (SS)",
+  Callback = function()
+    loadstring(game:GetObjects("rbxassetid://6695644299")[1].Source)()
+  end    
 })
 
 Local:AddButton({
-	Name = "Anti Fling",
-	Callback = function()
+  Name = "XRay",
+  Callback = function()
+    loadstring(game:HttpGet("https://pastefy.app/wmcnqSrX/raw"))()
+  end    
+})
+
+Local:AddButton({
+  Name = "Anti Fling",
+  Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Monster-Scripts/Anti-Fling/main/FE"))()
-  	end    
+    end    
 })
 
 Local:AddButton({
-	Name = "ClickTP",
-	Callback = function()
+  Name = "ClickTP",
+  Callback = function()
         loadstring(game:HttpGet("https://pastefy.app/I5BNncKK/raw"))()
-  	end    
+    end    
 })
 
 Local:AddButton({
-	Name = "Explorer",
-	Callback = function()
+  Name = "Explorer",
+  Callback = function()
         loadstring(game:GetObjects("rbxassetid://418957341")[1].Source)()
-  	end    
+    end    
 })
 
 Local:AddButton({
-	Name = "Infinite Jump",
-	Callback = function()
+  Name = "Infinite Jump",
+  Callback = function()
         loadstring(game:HttpGet("https://pastefy.app/w4LIHcTO/raw"))()
-  	end    
+    end    
 })
 
 Local:AddButton({
-	Name = "ESP",
-	Callback = function()
-        loadstring(game:HttpGet("https://pastefy.app/UqkSMwcP/raw"))()
-  	end    
+  Name = "ESP",
+  Callback = function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/DDFLBJns"))()
+    end    
 })
 
 Local:AddButton({
-	Name = "Invisibility",
-	Callback = function()
+  Name = "Invisibility",
+  Callback = function()
         loadstring(game:HttpGet(('https://raw.githubusercontent.com/0Ben1/fe/main/Protected - 2023-05-28T225112.055.lua.txt'),true))()
-  	end    
+    end    
 })
 
 Local:AddButton({
-	Name = "Fly",
-	Callback = function()
+  Name = "Fly",
+  Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Flexzstr/FlyScript/main/Beta"))()
-  	end    
+    end    
 })
 
 Local:AddButton({
-	Name = "Hitbox Expander",
-	Callback = function()
+  Name = "Hitbox Expander",
+  Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/HitboxExpander.lua"))()
-  	end    
+    end    
 })
 
 Local:AddButton({
-	Name = "Shiftlock",
-	Callback = function()
+  Name = "Shiftlock",
+  Callback = function()
         loadstring(game:HttpGet('https://pastebin.com/raw/WQ9NPeDS'))();
-  	end    
+    end    
 })
 
 Local:AddButton({
-	Name = "Server Stat Icons",
-	Callback = function()
+  Name = "Server Stat Icons",
+  Callback = function()
         loadstring(game:HttpGet("https://pastebin.com/raw/qSQKxrkt", true))()
     end    
 })
 
 Local:AddButton({
     Name = "Chat Bypass",
-	Callback = function()
+  Callback = function()
         loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/loader.lua"))({
             Settings = {
                 Mode = "Normal", -- Type Normal for the user interface. Type "simple" to get a more simple version of BetterBypasser
@@ -228,7 +229,7 @@ Local:AddButton({
 Local:AddButton({
     Name = "Auto Clicker",
     Callback = function()
-        loadstring(game:HttpGet("https://pastefy.app/bARQxzVL/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/bARQxzVL/raw"))()
       end    
 })
 
@@ -236,27 +237,27 @@ Local:AddButton({
 
 
 local Character = Window:MakeTab({
-	Name = "Character",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+  Name = "Character",
+  Icon = "rbxassetid://4483345998",
+  PremiumOnly = false
 })
 
 local Section = Character:AddSection({
-	Name = "Character Customazition"
+  Name = "Character Customazition"
 })
 
 Character:AddButton({
-	Name = "Headless [R6 & R15]",
-	Callback = function()
+  Name = "Headless [R6 & R15]",
+  Callback = function()
         loadstring(game:HttpGet("https://pastefy.app/9pm9Jes2/raw"))()
-  	end    
+    end    
 })
 
 Character:AddButton({
-	Name = "Korblox [R15 ONLY]",
-	Callback = function()
+  Name = "Korblox [R15 ONLY]",
+  Callback = function()
         loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-Ken-Koblox-and-Headless-respawn-6712"))()
-  	end    
+    end    
 })
 
 local Section = Character:AddSection({
@@ -265,8 +266,8 @@ local Section = Character:AddSection({
 
 local HubGui = Window:MakeTab({
     Name = "Hubs and GUIs",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+  Icon = "rbxassetid://4483345998",
+  PremiumOnly = false
 })
 
 local Section = HubGui:AddSection({
@@ -286,62 +287,109 @@ HubGui:AddButton({
       end    
 })
 
+HubGui:AddButton({
+    Name = "Ez Hub",
+    Callback = function()
+      loadstring(game:HttpGet(('https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua'),true))()
+    end
+})
 
+HubGui:AddButton({
+    Name = "Fire X Hub",
+    Callback = function()
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/tyreltrijo/firex/main/firex'))()
+    end
+})
+
+HubGui:AddButton({
+    Name = "VHub",
+    Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/V31nc/On-Discord/Created/VexHub"))()
+    end
+})
+
+HubGui:AddButton({
+    Name = "Ghost Hub",
+    Callback = function()
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/GhostHub'))()
+    end
+})
+
+HubGui:AddButton({
+    Name = "Remote Hub",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/zephyr10101/RemoteHub/main/Main",true))()
+    end
+})
+
+HubGui:AddButton({
+    Name = "Game Hub",
+    Callback = function()
+        loadstring(game:HttpGet("https://github.com/GamerScripter/Game_Hub_v2/raw/main/loader"))()
+    end
+})
+
+HubGui:AddButton({
+    Name = "Game Hub",
+    Callback = function()
+        loadstring(game:HttpGet("https://github.com/GamerScripter/Game_Hub_v2/raw/main/loader"))()
+    end
+})
 
 local Admins = Window:MakeTab({
-	Name = "Admins",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+  Name = "Admins",
+  Icon = "rbxassetid://4483345998",
+  PremiumOnly = false
 })
 
 local Section = Admins:AddSection({
-	Name = "Total Admin Scripts: 6"
+  Name = "Total Admin Scripts: 6"
 })
 
 Admins:AddParagraph("Read!","These are the most popular and most used admin scripts! Other admins script that are lesser-known will not be added.")
 
 local Section = Admins:AddSection({
-	Name = "Admin Scipts"
+  Name = "Admin Scipts"
 })
 
 Admins:AddButton({
-	Name = "Infinite Yield",
-	Callback = function()
+  Name = "Infinite Yield",
+  Callback = function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua'))()
-  	end    
+    end    
 })
 
 Admins:AddButton({
-	Name = "Nameless Admin",
-	Callback = function()
+  Name = "Nameless Admin",
+  Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"))()
-  	end    
+    end    
 })
 
 Admins:AddButton({
-	Name = "Shattervest Admin",
-	Callback = function()
+  Name = "Shattervest Admin",
+  Callback = function()
         loadstring(game:HttpGetAsync("https://pastebin.com/raw/Caniwq2N"))()
     end
 })
 
 Admins:AddButton({
-	Name = "CMD-X",
-	Callback = function()
+  Name = "CMD-X",
+  Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
     end
 })
 
 Admins:AddButton({
-	Name = "Reviz Admin",
-	Callback = function()
+  Name = "Reviz Admin",
+  Callback = function()
         loadstring(game:HttpGet(('https://pastebin.com/raw/pyzjWNhk'), true))()
     end
 })
 
 Admins:AddButton({
-	Name = "Fates Admin",
-	Callback = function()
+  Name = "Fates Admin",
+  Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))();
     end
 })
