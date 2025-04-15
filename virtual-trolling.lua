c2a3224392f779797c5ee9897fb92938ede75416
iiTzBenni <161487331+iiTzBenni@users.noreply.github.com>
2025-04-15 21:00:55 +0700

    virtual-trolling.lua

--- virtual-trolling.lua
+++ virtual-trolling.lua
@@ -1,7 +1,19 @@
 --Library
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 -- Window
-local Window = OrionLib:MakeWindow({Name = "V1rtual Hub - FE ScriptHub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "V1rtual Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", IconText = "Virtual Hub"})
+
+--[[
+Name = <string> - The name of the UI.
+HidePremium = <bool> - Whether or not the user details shows Premium status or not.
+SaveConfig = <bool> - Toggles the config saving in the UI.
+ConfigFolder = <string> - The name of the folder where the configs are saved.
+IntroEnabled = <bool> - Whether or not to show the intro animation.
+IntroText = <string> - Text to show in the intro animation.
+IntroIcon = <string> - URL to the image you want to use in the intro animation.
+Icon = <string> - URL to the image you want displayed on the window.
+CloseCallback = <function> - Function to execute when the window is closed.
+]]
 --notification
 OrionLib:MakeNotification({
   Name = "Thanks for using!",
